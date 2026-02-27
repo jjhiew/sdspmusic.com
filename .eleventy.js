b03ad3843e7f8cb4e62157eb691b4f68eb5600fe
JJ Hiew <jj@getquorum.com>
2026-02-27 17:23:30 -0500

    Reorg folders

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,9 @@
 module.exports = function(eleventyConfig) {
+  // Copy assets
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/*.png");
-  eleventyConfig.addPassthroughCopy("src/favicon.ico");
-  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
-  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
-  eleventyConfig.addPassthroughCopy("src/CNAME");
+  
+  // Copy static root files (favicons, CNAME, etc.) to the root of the output
+  eleventyConfig.addPassthroughCopy({ "src/static": "/" });
 
   return {
     dir: {
